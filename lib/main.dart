6e77e604eb2846b702e34aca64b5fd88da11c201
Walter Bucolo <bucolowalter@gmail.com>
2023-05-31 14:00:14 -0300

    Skipping tests

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:holdam_mobile/constants.dart';
-import 'package:holdam_mobile/widget/timer.dart';
-import 'package:holdam_mobile/widget/coupon_list.dart';
 import 'package:holdam_mobile/widget/signin.dart';
 
 void main() async {
