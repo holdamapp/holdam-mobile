cfbd49b7dc7d84fb221bc7a839f9567f519af4e8
Walter Bucolo <bucolowalter@gmail.com>
2023-05-31 13:05:12 -0300

    Testing2

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Holdam App Testing',
+      title: 'Holdam App Testing.',
 
       home: Scaffold(
           appBar: AppBar(
